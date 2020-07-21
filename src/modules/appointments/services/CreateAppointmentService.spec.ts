1f4fa195d472368f5b13e23a011a443764cb0612
Joao Bastos <japbastos@gmail.com>
2020-07-21 18:56:18 -0300

    User controllers, services and tests refactored

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,15 +1,20 @@
 import AppError from '@shared/errors/AppError';
 
-import FakeAppointmnetsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
-describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', async () => {
-    const fakeAppointmnetsRepository = new FakeAppointmnetsRepository();
-    const createAppointment = new CreateAppointmentService(
-      fakeAppointmnetsRepository,
-    );
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let createAppointment: CreateAppointmentService;
 
+describe('CreateAppointment', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
       provider_id: 'popo',
@@ -20,11 +25,6 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments on the same time', async () => {
-    const fakeAppointmnetsRepository = new FakeAppointmnetsRepository();
-    const createAppointment = new CreateAppointmentService(
-      fakeAppointmnetsRepository,
-    );
-
     const appointmentDate = new Date(2020, 4, 10, 11);
 
     await createAppointment.execute({
@@ -32,7 +32,7 @@ describe('CreateAppointment', () => {
       provider_id: 'popo',
     });
 
-    expect(
+    await expect(
       createAppointment.execute({
         date: appointmentDate,
         provider_id: 'popo',
